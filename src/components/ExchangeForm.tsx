14b98ea92c5dc84a24a9344b71819c0af0c31cdd
Amybuild <tondyorfaq@gmail.com>
2025-08-03 19:42:12 +0700

    [dyad] Скорректированы стили вкладок для прямых внешних краев и наклонного разделения, а также добавлено overflow-hidden к родительской карточке. - wrote 2 file(s)

--- src/components/ExchangeForm.tsx
+++ src/components/ExchangeForm.tsx
@@ -254,20 +254,20 @@ export function ExchangeForm({ onExchangeSuccess }: ExchangeFormProps) {
               <TabsTrigger 
                 value="bank" 
                 className="text-lg py-3 px-6 transition-all duration-300 ease-in-out
-                           transform skew-x-[6deg] -mr-1 /* Negative margin for overlap */
+                           transform skew-x-[2deg] -mr-1 /* Negative margin for overlap */
                            data-[state=active]:bg-gradient-to-b data-[state=active]:from-green-400 data-[state=active]:to-green-700 data-[state=active]:text-white data-[state=active]:shadow-lg data-[state=active]:z-20
                            data-[state=inactive]:bg-gradient-to-b data-[state=inactive]:from-red-400 data-[state=inactive]:to-red-700 data-[state=inactive]:text-white data-[state=inactive]:opacity-75 data-[state=inactive]:shadow-sm data-[state=inactive]:z-10"
               >
-                <span className="inline-block -skew-x-[6deg]">На банковский счет</span>
+                <span className="inline-block -skew-x-[2deg]">На банковский счет</span>
               </TabsTrigger>
               <TabsTrigger 
                 value="cash" 
                 className="text-lg py-3 px-6 transition-all duration-300 ease-in-out
-                           transform -skew-x-[6deg] -ml-1 /* Negative margin for overlap */
+                           transform -skew-x-[2deg] -ml-1 /* Negative margin for overlap */
                            data-[state=active]:bg-gradient-to-b data-[state=active]:from-green-400 data-[state=active]:to-green-700 data-[state=active]:text-white data-[state=active]:shadow-lg data-[state=active]:z-20
                            data-[state=inactive]:bg-gradient-to-b data-[state=inactive]:from-red-400 data-[state=inactive]:to-red-700 data-[state=inactive]:text-white data-[state=inactive]:opacity-75 data-[state=inactive]:shadow-sm data-[state=inactive]:z-10"
               >
-                <span className="inline-block skew-x-[6deg]">Наличными (доставка)</span>
+                <span className="inline-block skew-x-[2deg]">Наличными (доставка)</span>
               </TabsTrigger>
             </TabsList>
             <TabsContent value="bank" className="mt-4 space-y-4 px-6"> {/* Добавлен px-6 для возврата отступов */}
