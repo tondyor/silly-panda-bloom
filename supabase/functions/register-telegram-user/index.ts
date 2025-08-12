eba62dd59d9413bfd4482e727400f22bc0804232
Amybuild <tondyorfaq@gmail.com>
2025-08-13 02:31:51 +0700

    [dyad] Исправление ошибки развертывания функции Supabase - wrote 1 file(s)

--- supabase/functions/register-telegram-user/index.ts
+++ supabase/functions/register-telegram-user/index.ts
@@ -3,7 +3,7 @@ import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 // @ts-ignore
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.45.0";
 // @ts-ignore
-import { HmacSha256 } from "https://deno.land/std@0.190.0/crypto/hmac.ts";
+import { HmacSha256 } from "https://deno.land/std@0.190.0/crypto/mod.ts";
 
 declare const Deno: {
   env: {
